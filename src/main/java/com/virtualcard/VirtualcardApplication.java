085ed982f7ebf234c7c9896214091224e72fe9cf
dinairton <dinairton@gmail.com>
2026-08-12 14:46:43 +0100

    initial version

---
+++ src/main/java/com/virtualcard/VirtualcardApplication.java
@@ -0,0 +1,13 @@
+package com.virtualcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualcardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualcardApplication.class, args);
+    }
+
+}
